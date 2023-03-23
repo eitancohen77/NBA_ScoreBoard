4a368105a9ba00984d47abf258477e1ffed96e1e
Eitan Cohen <eitancohen77@gmail.com>
2023-03-23 15:10:57 -0400

    Added commuincation form python to javascript

--- node.js
+++ node.js
@@ -6,7 +6,6 @@ const data_out = 'Send this to python script'
 console.log('Data sent to python', data_out)
 const python_process = spawner('python', ['./nba.py', data_out])
 
-let json_data = []
 python_process.stdout.on('data', (data) => {
     const output = JSON.parse(data)
     console.log('Data recieved from python script', output)
